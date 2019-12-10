0d8482af00f6b97c14dd9f98e68f15ea64fabe37
jcereme <josue.cereme@capgemini.com>
2019-12-10 16:50:11 +0100

    codecov

---
+++ myerp-model/src/test/java/com/dummy/myerp/model/bean/comptabilite/SequenceEcritureComptableTest.java
@@ -0,0 +1,24 @@
+package com.dummy.myerp.model.bean.comptabilite;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class LigneComptableTest {
+
+
+	
+	@Test
+	public void toStringTestNull() {
+		SequenceEcritureComptable seq = null;
+	           
+		assertEquals(null,seq.toString());
+	}
+
+	}
